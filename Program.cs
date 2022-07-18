29079c9636ba3edc60a57eda3f92a6e381aecb02
Sorot Charoensinchai <86677452+taemoolopliam@users.noreply.github.com>
2022-07-18 10:09:24 +0700

    Enable CORS

--- Program.cs
+++ Program.cs
@@ -4,9 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+   options.AddDefaultPolicy(
+       policy =>
+       {
+           policy.WithOrigins("http://localhost:3000/")
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+       });
+});
 
 var app = builder.Build();
-
+app.UseCors();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
